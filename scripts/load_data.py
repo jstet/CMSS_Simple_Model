9d5c2c64aed87aa9442fd2ba9c5a67483553cb60
Jonas Stettner <mail@jstet.net>
2023-01-29 15:18:05 +0100

    working on presentation, imporving model

---
+++ scripts/load_data.py
@@ -0,0 +1,9 @@
+import networkx as nx
+
+G_hamst = nx.read_edgelist("data/soc-hamsterster.edges", comments="%")
+
+
+
+
+
+
--- scripts/model.py
+++ scripts/model.py
@@ -5,14 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def simple_model(g, upper_phi, alpha, beta):
+def simple_model(n, z, phi, upper_phi):
+    # uniform random graph
+    g = nx.fast_gnp_random_graph(n, z/n)
     # population is initially all-off
     nx.set_node_attributes(g, 0, "state")
 
     # each agent is assigned a threshold  drawn at random from a distribution
     for i in list(g.nodes):
-        phi = np.random.beta(alpha, beta)
-        attrs = {i: {"phi": 1}}
+        attrs = {i: {"phi": phi}}
         nx.set_node_attributes(g, attrs)
 
     # at time t = 0 a small fraction of vertices is switched on, fraction equals uppercase phi
@@ -25,28 +26,31 @@ def simple_model(g, upper_phi, alpha, beta):
     node_lst = list(g.nodes)
     # all vertices update their states in random, asynchronous order
     # (drawing a random index and removing the corresponding node from list)
-    while True:
-        if not node_lst:
-            break
-        idx = random.randint(0, len(node_lst)-1)
-        node = node_lst.pop(idx)
-        if g.nodes[node]["state"] != 1:
-            # An individual agent observes the current states of k other agents, which we call its neighbors
-            neighbours = g.adj[node]
-            k = len(neighbours)
-            phi = g.nodes[node]["phi"]
-            # (Counting neighbours with state = 1)
-            count = sum([1 for neighbour in list(neighbours) if g.nodes[neighbour]["state"] == 1])
-            try:
-                # Adopts state 1 if at least a threshold fraction of its neighbors are in state 1
-                if count/k >= phi:
-                    attrs = {node: {"state": 1}}
-                    nx.set_node_attributes(g, attrs)
-            except ZeroDivisionError as e:
-                pass
+    change = True
+    while change:
+        while True:
+            if not node_lst:
+                break
+            change = False
+            idx = random.randint(0, len(node_lst)-1)
+            node = node_lst.pop(idx)
+            if g.nodes[node]["state"] != 1:
+                # An individual agent observes the current states of k other agents, which we call its neighbors
+                neighbours = g.adj[node]
+                k = len(neighbours)
+                phi = g.nodes[node]["phi"]
+                # (Counting neighbours with state = 1)
+                count = sum([1 for neighbour in list(neighbours) if g.nodes[neighbour]["state"] == 1])
+                try:
+                    # Adopts state 1 if at least a threshold fraction of its neighbors are in state 1
+                    if count/k >= phi:
+                        attrs = {node: {"state": 1}}
+                        nx.set_node_attributes(g, attrs)
+                        change = True
+                except ZeroDivisionError as e:
+                    pass
 
     return initial, g
 
 
-
-#%%
+initial, end = simple_model(100,4, 0.3, upper_phi=2)
